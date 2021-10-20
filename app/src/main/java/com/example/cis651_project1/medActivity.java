1b6b4a8ec8bb4cd6a37bd8ec3ab08ece61765eb1
Ramesh Narasimman <rpattark@g.syr.edu>
2021-10-20 00:09:32 -0700

    updates to activities

--- app/src/main/java/com/example/cis651_project1/medActivity.java
+++ app/src/main/java/com/example/cis651_project1/medActivity.java
@@ -1,12 +1,46 @@
 package com.example.cis651_project1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 public class medActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.med_activity);
+
+        GridLayout gridLayout = findViewById(R.id.medLayout);
+        int column = getIntent().getIntExtra("column",4);
+        gridLayout.setColumnCount(column);
+        for (int i=0; i<column*column; i++){
+            LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    1.0f
+            );
+
+            ImageView imageView = new ImageView(this);
+            imageView.setLayoutParams(param);
+            imageView.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.qmark, null));
+            imageView.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.border, null));
+            gridLayout.addView(imageView);
+            final int finalI = i;
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(medActivity.this, "i="+ finalI, Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
     }
 }
